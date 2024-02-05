a99f3fdd3b827e104678275ddbe9f3b3c0bc1407
iramirneto <netoi@kean.edu>
2024-02-04 20:37:06 -0500

    Second Commit

---
+++ Average/NumAve.cs
@@ -0,0 +1,31 @@
+﻿//Iramir Neto
+//CPS3330*01
+//Lab #2
+
+using System;
+
+namespace Average
+{
+    public class NumAve
+    {
+        public static double MyAve(double num1, double num2)
+        {
+            double ave;
+            ave = (num1 + num2) / 2;
+
+            Console.WriteLine("The average of " + num1 + " and " + num2 +
+                " is " + ave);
+            return ave;
+        }
+
+        public static double MyAve(double num1, double num2, double num3)
+        {
+            double ave;
+            ave = (num1 + num2 + num3) / 3;
+
+            Console.WriteLine("The average of " + num1 + ", " + num2 +
+                " and " + num3 + " is " + ave);
+            return ave;
+        }
+    }
+} 
---
+++ TestAverage/Program.cs
@@ -0,0 +1,62 @@
+﻿using Average;
+
+namespace TestRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, ave, user;
+            do
+            {
+                Console.WriteLine("Average Calculation:");
+                Console.Write("How many number do you want to enter " +
+                    "(2 or 3):");
+                user = Convert.ToDouble(Console.ReadLine());
+
+                if (user == 2)
+                {
+                    do
+                    {
+                        Console.Write("Enter first number: ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Enter the second number: ");
+                        b = Convert.ToDouble(Console.ReadLine());
+                        if (a < 0 || b < 0)
+                            Console.WriteLine("Please enter a positive number!");
+                    } while (a < 0 || b < 0);
+
+                    ave = Average.NumAve.MyAve(a, b);
+                }
+                else if (user == 3)
+                {
+                    do
+                    {
+                        Console.Write("Enter first number: ");
+                        a = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Enter the second number: ");
+                        b = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Enter the third number: ");
+                        c = Convert.ToDouble(Console.ReadLine());
+                        if (a < 0 || b < 0 || c < 0)
+                            Console.WriteLine("Please enter a positive number!");
+                    } while (a < 0 || b < 0 || c < 0);
+
+                    ave = Average.NumAve.MyAve(a, b, c);
+                }
+                else
+                {
+                    Console.Write("Only calculates the average of 2 or 3 numbers.");
+                }
+
+                // Ask the user if they want to calculate again
+                Console.Write("Do you want to calculate again? (yes/no): ");
+                string response = Console.ReadLine().ToLower();
+
+                if (response != "yes")
+                    break;
+
+            } while (true);
+        }
+    }
+}
